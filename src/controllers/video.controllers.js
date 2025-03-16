de887fe7b7d96b7fd96351864ab0b6484eb02c7d
Bernard <bernarddira@gmail.com>
2025-03-16 17:59:16 +0200

    "Updated video controller"

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import mongoose from "mongoose";
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -86,24 +87,108 @@ const publishAVideo = asyncHandler(async(req, res) => {
 
 const getVideoById = asyncHandler(async (req, res) => {
    try {
-    const { videoId } = req.body;
+    const { videoId } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+    const video = await Videos.findById(videoId);
+
+    if(!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    return res
+    .status(201)
+    .json(new ApiResponse(200, video, "Video fetched successfully"))
    } catch (error) {
-    
+    throw new ApiError(500, error?.message || "Error while fetching video");
    }
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    try {
+        const { videoId } = req.params;
+        const {title, description } = req.body;
+        const thumbnailFile = req.file?.path; // Extract thumbnail
 
+        if(!mongoose.Types.ObjectId.isValid(videoId)) {
+            throw new ApiError(401, "Invalid video Id");
+        }
+
+        const video = await Videos.findOne({ _id: videoId, owner: req.user._id });
+
+        if(!video) {
+            throw new ApiError(404, "Video not found or you not authorized to update this video")
+        }
+
+        if(title) video.title = title;
+        if(description) video.description = description;
+        if(thumbnailFile) {
+            const thumbnailUploadResponse = await uploadOnCloudinary(thumbnailFile);
+            if(!thumbnailUploadResponse?.url) {
+                throw new ApiError(500, "Failed to upload thumbnail");
+            }
+            video.thumbnail = thumbnailUploadResponse.url;
+            }
+        await video.save();
+
+        return res
+        .status(200)
+        .json(new ApiResponse(201, "Video updated successfully"));
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Error while updating video")
+    }
 })
 
 const deleteVideo = asyncHandler( async(req, res) => {
-    const { videoId } = req.params
+    try {
+        const { videoId } = req.params;
+
+        if(!mongoose.Types.ObjectId.isValid(videoId)) {
+            throw new ApiError(400, "Invalid video Id");
+        }
+
+
+        const video = await Videos.findOne({ _id: videoId, owner: req._user._id })
+
+        if(!video) {
+            throw new ApiError(404, "video not found or you are not athorized to delete the video")
+        }
+
+        await Videos.deleteOne({ _id: videoId });
+
+        return res.status(200).json(new ApiResponse(200,  "Video deleted successfully"))
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Error while deleting a video")
+    }
 
 })
 
-const togglePublishDtatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+const togglePublishStatus = asyncHandler(async (req, res) => {
+   try {
+    const { videoId } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid video Id");
+    }
+
+    if(!videoId) {
+        throw new ApiError(404, "Video not found or you not athorized to toggle publish status")
+    }
+
+    const video = await Videos.findOne({ _id: videoId, owner: req.user._id});
+
+    video.isPublished = !video.isPublished;
+    await video.save()
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Publish status toggled successfully"));
+   } catch (error) {
+    throw new ApiError(500, error?.message || "Error while toggling publish status")
+   }
 
 })
 
@@ -113,5 +198,5 @@ export{
     getVideoById,
     updateVideo,
     deleteVideo,
-    togglePublishDtatus
+    togglePublishStatus
 }
