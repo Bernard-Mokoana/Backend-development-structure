de4754f81c1cdfaf879e32fd383218a53a4bf1bc
Bernard <bernarddira@gmail.com>
2025-03-22 12:22:03 +0200

    "Defined all endpoints"

--- src/app.js
+++ src/app.js
@@ -25,6 +25,10 @@ import { errorHandler } from "./middleware/error.middlewares.js"; // Global erro
 import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 // Define routes
 app.use("/api/v1/healthcheck", healthcheckRouter); // Health check endpoint
@@ -32,6 +36,10 @@ app.use("/api/v1/users", userRouter); // User-related endpoints
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 
 // Global error handler middleware
 app.use(errorHandler);
